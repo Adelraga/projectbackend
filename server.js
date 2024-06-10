e72ed7de8eb4d4e6a6bad00d5370a6109a1f6512
Adelraga <ar038122@gmail.com>
2024-06-10 20:37:55 +0300

    solve worker error

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(
   session({
     secret: "secret",
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+    cookie: { maxAge:  9 * 30 * 24 * 60 * 60 * 1000 },
     resave: false,
     saveUninitialized: false,
     store: store,
